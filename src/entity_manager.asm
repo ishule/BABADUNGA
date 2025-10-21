742de9c7f213600bdabed439fcd23c964ad9141c
atg68-ua <atg68@alu.ua.es>
2025-10-21 09:28:49 +0200

    Commented entity manager structure

--- src/entity_manager.asm
+++ src/entity_manager.asm
@@ -1,33 +1,26 @@
 INCLUDE "consts.inc"
-
-;; [ACTIVE] [TYPE] [FLAGS]   [----]
-SECTION "Entity Manager Data"        , WRAM0[$C000]
-component_info::      DS CMP_TOTALBYTES
-num_entities_alive::  DS 1	;; Contador de entidades activas
-next_free_entity::    DS 1		;; Índice de la siguiente entidad
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; =========== Entities array structure ===========
-;;
-;; [p_y_sprite] [p_x_sprite] [tile]    [ATTR]
-SECTION "Entity Sprites"             , WRAM0[$C100]
-component_sprite::    DS CMP_TOTALBYTES
-
-;; [p_y_low]    [p_x_low]    [health] 	[damage]
-SECTION "Entity Physics Position"    , WRAM0[$C200]
-component_physics_p:: DS CMP_TOTALBYTES
-
-;; [v_y_high]   [v_x_high]   [v_y_low] [v_x_low]
-SECTION "Entity Physics Velocity"    , WRAM0[$C300]
-component_physics_v:: DS CMP_TOTALBYTES
-
-;; [a_y_high]   [a_x_high]   [a_y_low] [a_x_low]
-SECTION "Entity Physics Acceleration", WRAM0[$C400]
-component_physics_a:: DS CMP_TOTALBYTES
-
-;; [y_collision_offset]   [x_collision_offset]   [height] [width]
-SECTION "Entity Physics Collisions", WRAM0[$C500]
-collision_values:: DS CMP_TOTALBYTES
+													;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+													;; =========== Entities array structure ===========
+													;;
+SECTION "Entity Manager Data"        , WRAM0[$C000] ;; [ACTIVE]     [TYPE]       [FLAGS]   [----]
+component_info::      DS CMP_TOTALBYTES             ;;
+num_entities_alive::  DS 1                          ;;
+next_free_entity::    DS 1                          ;;
+                                                    ;;
+SECTION "Entity Sprites"             , WRAM0[$C100] ;; [p_y_high]   [p_x_high]   [tile]    [ATTR]
+component_sprite::    DS CMP_TOTALBYTES             ;;
+                                                    ;;
+SECTION "Entity Physics Position"    , WRAM0[$C200] ;; [p_y_low]    [p_x_low]    [health]  [damage]
+component_physics_p:: DS CMP_TOTALBYTES             ;;
+                                                    ;;
+SECTION "Entity Physics Velocity"    , WRAM0[$C300] ;; [v_y_high]   [v_x_high]   [v_y_low] [v_x_low]
+component_physics_v:: DS CMP_TOTALBYTES             ;;
+                                                    ;;
+SECTION "Entity Physics Acceleration", WRAM0[$C400] ;; [a_y_high]   [a_x_high]   [a_y_low] [a_x_low]
+component_physics_a:: DS CMP_TOTALBYTES             ;;
+                                                    ;;
+SECTION "Entity Physics Collisions"  , WRAM0[$C500] ;; [y_collision_offset] [x_collision_offset] [height] [width]
+collision_values::    DS CMP_TOTALBYTES             ;;
 
 SECTION "Entity Manager Code", ROM0
 
