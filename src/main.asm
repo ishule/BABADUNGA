6ebb381dd19512b642866d808fcaa465fd36418b
atg68-ua <atg68@alu.ua.es>
2025-10-28 17:11:33 +0100

    basura de main quitada

--- src/main.asm
+++ src/main.asm
@@ -4,83 +4,13 @@ SECTION "Entry point", ROM0[$150]
 main::
    ld hl,rBGP
    ld [hl],%11100001
-   jp provisional_game_loop
-   
-   call turn_screen_off
-   ld hl,mapTiles
-   ld de,$8000
-   ld b, 12 * $10
-   call memcpy_256
-   
-   call InitDmaCopy
-   call sys_sound_init
-   call sys_sound_init_snake_music
-   call draw_map_spider
-   call turn_screen_on
-   call init_all_sprites
-
-   call man_entity_init ; Inicializar gestor de entidades
-   
-   call init_player
-   call init_gorilla
-   ;call init_snake
-   ;call init_spider
-   ;call init_verja
-   call init_invincibility
-
-   ;call open_door Esto se llama una vez el boss ha muerto
-   call joypad_init
-
-   call init_bullets
-
-
-   ;debug
-   ;ld a, $00
-   ;call man_entity_locate
-   
-   ;ld bc, $8A0A
-   ;ld d, $02
-   ;call change_entity_group_pos
-   
-   ;ld a, $00
-   ;call man_entity_locate
-
-   ;ld bc, $0001
-   ;ld d, $02
-   ;call change_entity_group_acc
-
-
+   jp game_loop
    game_loop:
-      call wait_vblank
-      call man_entity_draw
-      call sys_sound_siguienteNota
-
-      call joypad_read
-      call process_input
-
-      call update_invincibility
-
-      call sys_collision_check_all
-      call sys_gorilla_movement
-      ;call sys_snake_movement
-      ;call spider_logic
-      
-      call compute_physics
-      call check_player_shot
-
-      jr game_loop 
-
-   di
-   halt
-
-
-   provisional_game_loop:
 
       call init_player_stats
       ; ======== DEBUG =========
-      ;call load_snake_screen
+      call load_snake_screen
       ;call load_spider_screen
-
       ; =======================
       call load_title_screen
       call load_tutorial_screen
@@ -98,7 +28,7 @@ main::
    .defeat
       call load_defeat_screen
    .end
-      jr provisional_game_loop
+      jr game_loop
 
 
    init_player_stats::
